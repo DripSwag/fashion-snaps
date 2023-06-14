8b2f236161ba634ddcb8583ab3dc4774383827eb
DripSwag <mimirani33@gmail.com>
2023-06-14 18:42:49 +0100

    "Login pushes to the homepage route on successful credentials"

--- frontend/app/LoginForm.tsx
+++ frontend/app/LoginForm.tsx
@@ -1,15 +1,19 @@
 'use client'
 
-import {useState} from "react"
+import {AppRouterInstance} from "next/dist/shared/lib/app-router-context"
+import {useRouter} from "next/navigation"
+import {Dispatch, SetStateAction, useState} from "react"
 
-async function clicked(username: string, password: string){
+async function clicked(username: string, password: string, router: AppRouterInstance, setIncorrect: Dispatch<SetStateAction<boolean>>){
   const response = await fetch('http://127.0.0.1:8000/api/login/' + username + '/' + password)
-  console.log(await response.json())
+  response.status === 202 ? router.push('/homepage') : setIncorrect(true)
 }
 
 export default function LoginForm(){
   const [username, setUsername] = useState<string>('')
   const [password, setPassword] = useState<string>('')
+  const [incorrect, setIncorrect] = useState(false)
+  const router = useRouter()
 
   return(
     <div>
@@ -17,7 +21,10 @@ export default function LoginForm(){
         <input placeholder='Username' type='text' onChange={(event) => { setUsername(event.target.value) }}></input>
         <input placeholder='Password' type='password' onChange={(event) => { setPassword(event.target.value) }}></input>
       </form>
-      <button onClick={() => { clicked(username, password) }}>Login</button>
+      <button onClick={() => { clicked(username, password, router, setIncorrect) }}>Login</button>
+      {
+        incorrect && <p>Credentials inccorect</p>
+      }
     </div>
   )
 }
