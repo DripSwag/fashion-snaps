973e6eef2fd51a1150a860c5259242bb7138f4b0
DripSwag <mimirani33@gmail.com>
2023-06-17 09:36:34 +0100

    "Fixed media root to stop /media/media"

--- fashion/settings.py
+++ fashion/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '')
-MEDIA_URL = '/media/'
+MEDIA_URL = '/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
