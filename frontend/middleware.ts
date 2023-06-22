9379fbe28721398513a0810d0c6f750d3c6bdf3d
DripSwag <mimirani33@gmail.com>
2023-06-22 17:36:13 +0100

    "Added middleware to add postId if missing"

---
+++ frontend/middleware.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+interface post{
+  id: number
+}
+
+async function getPostId(){
+  const response = await fetch('http://127.0.0.1:8000/api/post/get')
+  if(response.status === 200){
+    const body: post = await response.json()
+    return body['id'].toString()
+  }
+}
+
+export async function middleware(request: NextRequest){
+  if(request.nextUrl.searchParams.get('postId') === null){
+    return NextResponse.redirect(new URL(request.nextUrl.pathname + '?postId=' + await getPostId(), request.url))
+  }
+}
+
+export const config = {
+  matcher: '/homepage/:path?'
+}
