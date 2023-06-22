dc189085fe2844fdf554eb9aa56de6730e7c581f
DripSwag <mimirani33@gmail.com>
2023-06-22 19:54:00 +0100

    "Added pulling posts to account page"

--- frontend/app/homepage/[userid]/account/page.tsx
+++ frontend/app/homepage/[userid]/account/page.tsx
@@ -1,5 +1,38 @@
-export default function Account(){
-  return(
-    <div>Account</div>
-  )
+import Post from "./Post";
+
+interface post {
+  id: number;
+  image: string;
+  user: number;
+}
+
+async function getPosts(userId: string) {
+  const response = await fetch(
+    "http://127.0.0.1:8000/api/post/user/get/" + userId
+  );
+  if (response.status === 200) {
+    const body = await response.json();
+    console.log(body);
+    return body;
+  }
+}
+
+export default async function Account({
+  params,
+}: {
+  params: { userid: string };
+}) {
+  const posts: Array<post> = await getPosts(params["userid"]);
+
+  return (
+    <main>
+      <h1>Account</h1>
+      <div>
+        {posts &&
+          posts.map((data: post) => {
+            return <Post img={data["image"]} id={data["id"]} />;
+          })}
+      </div>
+    </main>
+  );
 }
