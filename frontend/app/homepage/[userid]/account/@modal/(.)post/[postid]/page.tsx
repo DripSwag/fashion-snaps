fe8f0725bd20731c944cb6d68d96282ff5c19b00
DripSwag <mimirani33@gmail.com>
2023-06-29 16:55:05 +0100

    "Fixed scaling of account posts"

--- frontend/app/homepage/[userid]/account/@modal/(.)post/[postid]/page.tsx
+++ frontend/app/homepage/[userid]/account/@modal/(.)post/[postid]/page.tsx
@@ -21,7 +21,7 @@ export default async function Post({ params }: { params: { postid: string } }) {
   return (
     <div className="w-screen h-screen bg-[#61616199] fixed top-0 left-0">
       <img
-        className="h-3/4 aspect-square pb-24 pt-4 px-8 bg-[#F8F8FF] relative left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2"
+        className="h-3/4 aspect-auto pb-24 pt-4 px-8 bg-[#F8F8FF] relative left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2"
         src={image}
       ></img>
     </div>
