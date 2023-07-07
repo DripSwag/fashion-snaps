beb31b7750d34b48c12fa6c8286d12a31a97576e
DripSwag <mimirani33@gmail.com>
2023-07-07 20:20:57 +0100

    Fixed styling for modal for account post

--- frontend/app/homepage/[userid]/account/@modal/(.)post/[postid]/page.tsx
+++ frontend/app/homepage/[userid]/account/@modal/(.)post/[postid]/page.tsx
@@ -20,10 +20,9 @@ export default async function Post({ params }: { params: { postid: string } }) {
 
   return (
     <div className="w-screen h-screen bg-[#61616199] fixed top-0 left-0">
-      <img
-        className="h-3/4 aspect-auto pb-24 pt-4 px-8 bg-[#F8F8FF] relative left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2"
-        src={image}
-      ></img>
+      <div className="h-[80%] w-fit absolute px-8 pt-4 left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 bg-[#f8f8ff]">
+        <img className="h-[80%] aspect-auto" src={image}></img>
+      </div>
     </div>
   );
 }
