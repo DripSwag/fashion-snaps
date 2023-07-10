04fd5e3f1cd97a40887905d71feda3c7031fd0e1
DripSwag <mimirani33@gmail.com>
2023-07-10 17:44:31 +0100

    Fixed routing of bookmark post

--- frontend/app/homepage/[userid]/collection/Post.tsx
+++ frontend/app/homepage/[userid]/collection/Post.tsx
@@ -10,7 +10,7 @@ export default function Post({
   userId: string;
 }) {
   return (
-    <Link href={"/homepage/" + userId} className="post">
+    <Link href={"/homepage/" + userId + "?postId=" + postId} className="post">
       <img
         src={"http://127.0.0.1:8000/" + image}
         className="w-[80%] aspect-auto relative left-1/2 -translate-x-1/2"
