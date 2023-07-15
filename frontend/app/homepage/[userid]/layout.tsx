6344408e1278fc473f0d0c2a3ab7c5cd0fd89ea4
DripSwag <mimirani33@gmail.com>
2023-07-15 17:57:31 +0100

    Fixed navbar to work with userQueuePost

--- frontend/app/homepage/[userid]/layout.tsx
+++ frontend/app/homepage/[userid]/layout.tsx
@@ -2,6 +2,24 @@ import React from "react";
 import Link from "next/link";
 import Logout from "./Logout";
 
+interface post {
+  id: number | null;
+}
+
+async function getHomepageUrl(userId: string) {
+  const body: post = await fetch("http://127.0.0.1:8000/api/post/get", {
+    cache: "no-store",
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ user: parseInt(userId) }),
+  }).then((response) => response.json());
+
+  const pathPostId = body.id !== undefined ? body.id?.toString() : "0";
+  return "/homepage/" + userId + "?postId=" + pathPostId;
+}
+
 export default async function HomepageLayout({
   children,
   params,
@@ -9,17 +27,15 @@ export default async function HomepageLayout({
   children: React.ReactNode;
   params: { userid: string };
 }) {
-  //Add middleware so I dont need to call get post because the cache is stored and not cleared because the nav is persistent
+  const homepageUrl: string = await getHomepageUrl(params.userid);
+
   return (
     <section className="h-screen w-full flex flex-col items-center">
       <nav className="w-full h-16 py-4 px-4 flex items-center gap-4 max-w-[1200px]">
-        <Link
-          href={`/homepage/${params["userid"]}`}
-          className="mx-4 font-bold text-xl"
-        >
+        <a href={homepageUrl} className="mx-4 font-bold text-xl">
           Fashion Snaps
-        </Link>
-        <Link href={`/homepage/${params["userid"]}`}>Homepage</Link>
+        </a>
+        <a href={homepageUrl}>Homepage</a>
         <Link href={`/homepage/${params["userid"]}/account`}>Account</Link>
         <a href={`/homepage/${params["userid"]}/collection`}>Collection</a>
         <Logout />
