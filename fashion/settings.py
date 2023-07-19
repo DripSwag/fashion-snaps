45522c89fe00bd69c96a5fee6d58e9d47d7ab3dc
DripSwag <mimirani33@gmail.com>
2023-07-19 20:50:40 +0100

    Debug set to true

--- fashion/settings.py
+++ fashion/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xqf_1&w#++%zmtbdcy5d0b!5ryolv!e6l8=$()@%w8xx447j00'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
