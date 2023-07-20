71dac3fac85f72820d20a3a9caf63dd320f2a834
DripSwag <mimirani33@gmail.com>
2023-07-20 17:12:42 +0100

    Added whitenoise dependency

--- fashion/settings.py
+++ fashion/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'fashion.urls'
